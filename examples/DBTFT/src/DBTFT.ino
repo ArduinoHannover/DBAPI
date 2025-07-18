7987a82acd92cd0a6bf0c8bdc5de82002f07be7f
Luca Schröder <git@sndstrm.de>
2025-07-18 16:41:01 +0200

    All in one version
    + WifiManager - configure your wifi, display rotation, station and train types
    + Default config for cheap ESP32 display module
    + OTA Update (or via WfiManager Update)
    + Auto brightness adjustment (when pin is set)
    - Dependency on 8266 Library, more flexibility

--- examples/DBTFT/src/DBTFT.ino
+++ examples/DBTFT/src/DBTFT.ino
@@ -1,14 +1,30 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <DBAPI.h>
 #include <Adafruit_ILI9341.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 #include <TimeLib.h>
 
-const char* ssid = "SSID";
-const char* password = "password";
-const char* fromStationName = "Hannnover Hbf";
+//#define BOARD_2432S028R //ESP32 + ILI9341 2.8" TFT
+#define AUTOCONFIG
+#define ENABLEOTA
+//#define DEBUG_BRIGHTNESS
+
+#ifdef AUTOCONFIG
+	const char* apSSID = "DBTafel";
+	const char* apPassword = "nichtPuenktlich";
+	#include <LittleFS.h>
+	#include <WiFiManager.h>
+#else
+	const char* ssid = "SSID";
+	const char* password = "password";
+#endif
+#ifdef ENABLEOTA
+	const char* otaPassword = "iliketrains";
+	#include <ArduinoOTA.h>
+#endif
+char fromStationName[40] = "Hannnover Hbf";
+uint16_t filter = PROD_RE | PROD_S;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org");
@@ -32,44 +48,44 @@ DBdeparr* departure = NULL;
 uint32_t nextCheck;
 uint32_t nextTime;
 uint32_t nextScroll;
+uint32_t nextBrightness;
 time_t old_time;
+bool updateInhibit = false;
+#ifdef ESP32
+	#define MAX_ANALOG 4095
+#else
+	#define MAX_ANALOG 1023
+#endif
+uint16_t currentBrightness = 0;
+uint8_t  rotation = 1;
 
-#define TFT_DC 16
-#define TFT_CS 15
-#define TFT_BL  5
+#ifdef BOARD_2432S028R
+	#define TFT_DC  2
+	#define TFT_CS 15
+	#define TFT_BL 21
+	#define LDR_IN 34
+	// GT36516 LDR -> GND, 1M Pullup, 1M Pulldown
+	// 1V dark
+	// 0V bright
+#else
+	#define TFT_DC 16
+	#define TFT_CS 15
+	#define TFT_BL  5
+	#define LDR_IN A0 // optional LDR
+#endif
+#define BUT_IN 0
+#define MIN_BRIGHTNESS   2
+#define MAX_BRIGHTNESS 255 // Nothing above noteworthy
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+#ifdef AUTOCONFIG
+#endif
 
 time_t getNtpTime() {
 	return timeClient.getEpochTime();
 }
 
-void setup() {
-	Serial.begin(115200);
-	tft.begin();
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	pinMode(TFT_BL, OUTPUT);
-	digitalWrite(TFT_BL, HIGH);
-	tft.fillScreen(BACKGROUND_COLOR);
-	tft.setTextColor(FOREGROUND_COLOR);
-	tft.setRotation(1);
-	tft.print("Verbinde...");
- 	Serial.print("Verbinde...");
-	while (WiFi.status() != WL_CONNECTED) {
-		tft.write('.');
-    	Serial.write('.');
-		delay(500);
-	}
-	db.setAGFXOutput(true);
-	Serial.println();
-	fromStation = db.getStation(fromStationName);
-	yield();
-	timeClient.begin();
-	timeClient.setTimeOffset(3600); // CET
-	adjustTime(3600);
-	setSyncProvider(getNtpTime);
-
-	// Draw static content
+void drawStaticContent() {
 	tft.fillScreen(BACKGROUND_COLOR);
 	tft.setTextColor(FOREGROUND_COLOR);
 	tft.setTextSize(2);
@@ -90,10 +106,273 @@ void setup() {
 	tft.print("Gleis");
 }
 
+#ifdef ENABLEOTA
+void updateStarted() {
+	updateInhibit = true;
+	Serial.println("Update started...");
+	tft.fillScreen(BACKGROUND_COLOR);
+	tft.setTextColor(FOREGROUND_COLOR);
+	tft.setCursor(0, 0);
+	tft.setTextSize(2);
+	tft.println("Update...");
+}
+#endif
+
+#ifdef AUTOCONFIG
+WiFiManager wifiManager;
+WiFiManagerParameter stationNameParameter("stationName", "Stationsname", fromStationName, sizeof(fromStationName));
+WiFiManagerParameter rotationParameter("rotation", "Rotation (1 oder 3)", String(rotation).c_str(), 1);
+WiFiManagerParameter filterICEParameter("filterICE", "Hochgeschwindigkeitsz&uuml;ge (1 zum Aktivieren)", String((filter & PROD_ICE) > 0).c_str(), 1);
+WiFiManagerParameter filterICECParameter("filterICEC", "Intercity- und Eurocityz&uuml;ge 1 zum Aktivieren)", String((filter & PROD_IC_EC) > 0).c_str(), 1);
+WiFiManagerParameter filterIRParameter("filterIR", "Interregio- und Schnellz&uuml;ge (1 zum Aktivieren)", String((filter & PROD_IR) > 0).c_str(), 1);
+WiFiManagerParameter filterREParameter("filterRE", "Nahverkehr, sonsitge Z&uuml;ge (1 zum Aktivieren)", String((filter & PROD_RE) > 0).c_str(), 1);
+WiFiManagerParameter filterSParameter("filterS", "S-Bahnen (1 zum Aktivieren)", String((filter & PROD_S) > 0).c_str(), 1);
+WiFiManagerParameter filterBUSParameter("filterBUS", "Busse (1 zum Aktivieren)", String((filter & PROD_BUS) > 0).c_str(), 1);
+WiFiManagerParameter filterSHIPParameter("filterSHIP", "Schiffe (1 zum Aktivieren)", String((filter & PROD_SHIP) > 0).c_str(), 1);
+WiFiManagerParameter filterUParameter("filterU", "U-Bahnen (1 zum Aktieren)", String((filter & PROD_U) > 0).c_str(), 1);
+WiFiManagerParameter filterSTBParameter("filterSTB", "Stra&szlig;enbahnen (1 zum Aktivieren)", String((filter & PROD_STB) > 0).c_str(), 1);
+WiFiManagerParameter filterASTParameter("filterAST", "Anrufpflichtige Verkehre (1 zum Aktivieren)", String((filter & PROD_AST) > 0).c_str(), 1);
+bool shouldSaveConfig = false;
+
+void configModeCallback (WiFiManager *myWiFiManager) {
+	tft.fillScreen(BACKGROUND_COLOR);
+	tft.setTextColor(FOREGROUND_COLOR);
+	tft.setTextSize(2);
+	tft.setCursor(0, 0);
+	tft.println("Zur Konfiguration mit\nAccess-Point verbinden:");
+	tft.println(apSSID);
+	tft.println(apPassword);
+	if (WiFi.isConnected()) {
+		tft.println("Oder im lokalen WLAN:");
+		tft.println(WiFi.localIP());
+	}
+}
+
+void failedConfig() {
+	tft.fillScreen(0xF800); // red
+	tft.setTextColor(FOREGROUND_COLOR);
+	tft.setTextSize(2);
+	tft.setCursor(0, 0);
+	tft.println("WLAN Verbindung fehl\n-geschlagen");
+	tft.println("Konfiguration nicht\nerfolgt");
+	tft.print("Starte neu...");
+	delay(5000);
+	ESP.restart();
+	delay(5000);
+}
+
+void saveConfigCallback () {
+	shouldSaveConfig = true;
+	Serial.println("New data to save");
+}
+
+void readParams() {
+	if (LittleFS.exists("/station")) {
+		File f = LittleFS.open("/station", "r");
+		if (f) {
+			uint8_t i = 0;
+			while (f.available() && i < sizeof(fromStationName)) {
+				fromStationName[i++] = f.read();
+			}
+			if (i < sizeof(fromStationName))
+			fromStationName[i] = 0;
+		} else {
+			Serial.println("File not open");
+		}
+	} else {
+		Serial.println("File not found");
+	}
+	if (LittleFS.exists("/rotation")) {
+		File f = LittleFS.open("/rotation", "r");
+		if (f) {
+			uint8_t r = f.read() - '0';
+			if (r == 1 || r == 3) {
+				rotation = r;
+			}
+		} else {
+			Serial.println("File not open");
+		}
+	} else {
+		Serial.println("File not found");
+	}
+	if (LittleFS.exists("/filter")) {
+		File f = LittleFS.open("/filter", "r");
+		if (f) {
+			String s = f.readString();
+			long val = s.toInt();
+			if (val > 0 && val <= 1023) {
+				filter = val;
+			} else {
+				Serial.print("Invalid filter value: ");
+				Serial.print(s);
+				Serial.print(" / ");
+				Serial.println(val);
+			}
+		} else {
+			Serial.println("File not open");
+		}
+	} else {
+		Serial.println("File not found");
+	}
+}
+
+void checkConfigRequest() {
+	if (!digitalRead(BUT_IN)) {
+		if (!wifiManager.startConfigPortal(apSSID, apPassword)) {
+			// exited
+		}
+		afterConfigCallback();
+		drawStaticContent();
+		updateInhibit = false;
+		nextCheck = 0;
+	}
+}
+
+void afterConfigCallback() {
+	if (shouldSaveConfig) {
+		{
+			strncpy(fromStationName, stationNameParameter.getValue(), sizeof(fromStationName));
+			Serial.print("Stationsname gesetzt: ");
+			Serial.println(fromStationName);
+			File f = LittleFS.open("/station", "w");
+			f.print(fromStationName);
+			f.close();
+		}
+		uint8_t r = rotationParameter.getValue()[0];
+		if (r == '1' || r == '3') {
+			rotation = r - '0';
+			File f = LittleFS.open("/rotation", "w");
+			f.write(r);
+			f.close();
+			tft.setRotation(rotation);
+			Serial.print("Rotation gesetzt: ");
+			Serial.println(rotation);
+		}
+		uint16_t val =
+			(filterICEParameter.getValue()[0]  == '1') * PROD_ICE |
+			(filterICECParameter.getValue()[0] == '1') * PROD_IC_EC |
+			(filterIRParameter.getValue()[0]   == '1') * PROD_IR |
+			(filterREParameter.getValue()[0]   == '1') * PROD_RE |
+			(filterSParameter.getValue()[0]    == '1') * PROD_S |
+			(filterBUSParameter.getValue()[0]  == '1') * PROD_BUS |
+			(filterSHIPParameter.getValue()[0] == '1') * PROD_SHIP |
+			(filterUParameter.getValue()[0]    == '1') * PROD_U |
+			(filterSTBParameter.getValue()[0]  == '1') * PROD_STB |
+			(filterASTParameter.getValue()[0]  == '1') * PROD_AST;
+		if (val > 0) {
+			File f = LittleFS.open("/filter", "w");
+			f.print(val);
+			f.close();
+			filter = val;
+			Serial.print("Filter gesetzt: ");
+			Serial.println(filter);
+		} else {
+			Serial.print("Filter setzen fehlgeschlagen: ");
+			Serial.println(val);
+		}
+	} else {
+		Serial.println("Nicht speichern.");
+	}
+}
+#endif
+
+void setup() {
+	Serial.begin(115200);
+	tft.begin();
+	WiFi.mode(WIFI_STA);
+	pinMode(BUT_IN, INPUT_PULLUP);
+#ifndef AUTOCONFIG
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+#else
+	if (!LittleFS.begin()) {
+		Serial.println("LittleFS mount failed");
+	} else {
+		readParams();
+	}
+	wifiManager.setAPCallback(configModeCallback);
+	stationNameParameter.setValue(fromStationName, sizeof(fromStationName));
+	wifiManager.addParameter(&stationNameParameter);
+	rotationParameter.setValue(String(rotation).c_str(), 1);
+	wifiManager.addParameter(&rotationParameter);
+	filterICEParameter.setValue(String((filter & PROD_ICE) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterICEParameter);
+	filterICECParameter.setValue(String((filter & PROD_IC_EC) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterICECParameter);
+	filterIRParameter.setValue(String((filter & PROD_IR) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterIRParameter);
+	filterREParameter.setValue(String((filter & PROD_RE) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterREParameter);
+	filterSParameter.setValue(String((filter & PROD_S) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterSParameter);
+	filterBUSParameter.setValue(String((filter & PROD_BUS) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterBUSParameter);
+	filterSHIPParameter.setValue(String((filter & PROD_SHIP) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterSHIPParameter);
+	filterUParameter.setValue(String((filter & PROD_U) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterUParameter);
+	filterSTBParameter.setValue(String((filter & PROD_STB) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterSTBParameter);
+	filterASTParameter.setValue(String((filter & PROD_AST) > 0).c_str(), 1);
+	wifiManager.addParameter(&filterASTParameter);
+	wifiManager.setSaveConfigCallback(saveConfigCallback);
+	wifiManager.setConnectTimeout(30);
+	wifiManager.setConfigPortalTimeout(120);
+#endif
+	pinMode(TFT_BL, OUTPUT);
+	digitalWrite(TFT_BL, HIGH);
+	tft.fillScreen(BACKGROUND_COLOR);
+	tft.setTextColor(FOREGROUND_COLOR);
+	tft.setRotation(rotation);
+	tft.setTextSize(2);
+	tft.print("Verbinde...");
+ 	Serial.print("Verbinde...");
+#ifdef AUTOCONFIG
+	if (!wifiManager.autoConnect(apSSID, apPassword)) {
+		afterConfigCallback(); // try to save either way 
+		failedConfig();
+	}
+	afterConfigCallback();
+#else
+	while (WiFi.status() != WL_CONNECTED) {
+		tft.write('.');
+    	Serial.write('.');
+		delay(500);
+	}
+#endif
+#ifdef ENABLEOTA
+	ArduinoOTA.onStart(updateStarted);
+	ArduinoOTA.onEnd([]() {
+		updateInhibit = false;
+		tft.println("Fertig, starte neu.");
+	});
+	ArduinoOTA.onError([](ota_error_t e) {
+		updateInhibit = false;
+		tft.println("Fehler.");
+		delay(5000);
+		drawStaticContent();
+	});
+	ArduinoOTA.setPassword(otaPassword);
+	ArduinoOTA.begin();
+#endif
+#ifdef ESP32
+	analogSetAttenuation(ADC_2_5db); // ESP8266 equivalent 1.1V max
+#endif
+	db.setAGFXOutput(true);
+	Serial.println();
+	fromStation = db.getStation(fromStationName);
+	yield();
+	timeClient.begin();
+	timeClient.setTimeOffset(3600); // CET
+	adjustTime(3600);
+	setSyncProvider(getNtpTime);
+	drawStaticContent();
+}
+
 uint32_t scroll;
 time_t tdst;
 void loop() {
-	if (nextTime < millis()) {
+	if (nextTime < millis() && !updateInhibit) {
 		timeClient.update();
 		time_t tnow = timeClient.getEpochTime();
 		tdst = dst(tnow);
@@ -111,12 +390,12 @@ void loop() {
 		}
 		nextTime = millis() + 1000;
 	}
-	if (nextCheck < millis()) {
+	if (nextCheck < millis() && !updateInhibit) {
 		if (WiFi.status() != WL_CONNECTED) {
 			Serial.println("Disconnected");
 		}
 		Serial.println("Reload");
-		da = db.getDepartures(fromStation->stationId, NULL, tdst, 11, 3, PROD_RE | PROD_S);
+		da = db.getDepartures(fromStation->stationId, NULL, tdst, 11, 3, filter);
 		Serial.println();
 		departure = da;
 		uint16_t pos = 21;
@@ -174,7 +453,7 @@ void loop() {
 			tft.setCursor(tft.width() - 7 * 6 * 2, pos);
 			if (strcmp("", departure->newPlatform) && strcmp(departure->platform, departure->newPlatform)) {
 				tft.setTextColor(HIGHLIGHT_COLOR);
-				tft.print("->");
+				//tft.print("->");
 				tft.print(departure->newPlatform);
 			} else {
 				tft.print(departure->platform);
@@ -189,7 +468,7 @@ void loop() {
 		nextCheck = millis() + 50000;
 	}
 #ifdef WIDE_MODE
-	if (nextScroll < millis()) {
+	if (nextScroll < millis() && !updateInhibit) {
 		departure = da;
 		uint16_t pos = 21;
 	    while (departure != NULL) {
@@ -199,6 +478,33 @@ void loop() {
 		}
 		scroll++;
 		nextScroll = millis() + SCROLL_INTERVAL;
+	}
+#endif
+#ifdef ENABLEOTA
+	ArduinoOTA.handle();
+#endif
+#ifdef AUTOCONFIG
+	checkConfigRequest();
+#endif
+#ifdef LDR_IN
+	if (nextBrightness < millis()) {
+		uint16_t b = analogRead(LDR_IN);
+		if (b < currentBrightness) {
+			currentBrightness--;
+		} else if (b > currentBrightness) {
+			currentBrightness++;
+		}
+		uint16_t brightness = map(currentBrightness, MAX_ANALOG, 0, MIN_BRIGHTNESS, MAX_BRIGHTNESS);
+		analogWrite(TFT_BL, brightness);
+#ifdef DEBUG_BRIGHTNESS
+		Serial.print("B:\t");
+		Serial.print(b);
+		Serial.print("\tCB:\t");
+		Serial.print(currentBrightness);
+		Serial.print("\tL:\t");
+		Serial.println(brightness);
+#endif
+		nextBrightness = millis() + 10;
 	}
 #endif
 }
