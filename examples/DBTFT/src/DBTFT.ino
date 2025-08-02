67d347bfc5df55231b1cb83d1492358f07e27ae3
Luca Schröder <git@sndstrm.de>
2025-08-02 21:03:06 +0200

    Adjust orange

--- examples/DBTFT/src/DBTFT.ino
+++ examples/DBTFT/src/DBTFT.ino
@@ -40,7 +40,7 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org");
 #define COLOR_BLACK  0x0000
 #define COLOR_BLUE   0x001F
 #define COLOR_YELLOW 0xFFE0
-#define COLOR_ORANGE 0xFDA0
+#define COLOR_ORANGE 0xFBE0
 
 // Optional view with large destinations, does interfere with platform when station names are longer
 #define WIDE_MODE
